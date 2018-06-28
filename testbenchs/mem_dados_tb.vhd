9fc463836d75ffa15dad985d2a85f63d1d40804c
Gabriel Araujo <gabriel.araujo.5000@gmail.com>
2018-06-28 17:07:53 -0300

    Completar test bench

--- testbenchs/mem_dados_tb.vhd
+++ testbenchs/mem_dados_tb.vhd
@@ -1,11 +1,53 @@
 library ieee ;
-	use ieee.std_logic_1164.all ;
-	use ieee.numeric_std.all ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
 
 entity mem_dados_tb is
 end entity ; -- mem_dados_tb
 architecture arch of mem_dados_tb is
-
+signal wren        : std_logic ;
+signal clock       : std_logic;
+signal address     : std_logic_vector (7 downto 0);
+signal data        : std_logic_vector (31 downto 0);
+signal q           : std_logic_vector (31 downto 0);
+constant CLK_PERIOD : time := 100 ps;
+component mem_dados
+port(
+    address     : IN std_logic_vector (7 downto 0);
+    clock       : IN std_logic;
+    data        : IN std_logic_vector (31 downto 0);
+    wren        : IN std_logic ;
+    q           : OUT std_logic_vector (31 downto 0)
+    );
+end component;
 begin
-
+    i1: mem_dados
+    port map (
+        address => address,
+        clock => clock,
+        data => data,
+        wren => wren,
+        q => q
+        );
+clock_process : process
+begin
+    clock <= '0';
+    wait for CLK_PERIOD/2;  --for half of clock period clock stays at '0'.
+    clock <= '1';
+    wait for CLK_PERIOD/2;  --for next half of clock period clock stays at '1'.
+end process;
+init : process
+begin
+    wren <= '1';
+    wait for 50 ps;
+    -- writing memory
+    for i in 0 to 100 loop
+        address <= std_logic_vector(to_unsigned(i, 8));
+        data    <= std_logic_vector(to_unsigned(i, 32));
+        wait for 50 ps;
+        assert (i = to_integer(unsigned(q))) report "Data memory read failure" severity failure;
+        wait for 50 ps;
+    end loop;
+wait;
+end process ; -- init
 end architecture ; -- arch
