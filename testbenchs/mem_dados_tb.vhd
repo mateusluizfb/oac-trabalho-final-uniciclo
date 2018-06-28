bf4197febe7982de242ab2848e8a4a31e44753be
Gabriel Araujo <gabriel.araujo.5000@gmail.com>
2018-06-28 14:29:28 -0300

    Adicionar test bench para memoria de dados

---
+++ testbenchs/mem_dados_tb.vhd
@@ -0,0 +1,11 @@
+library ieee ;
+	use ieee.std_logic_1164.all ;
+	use ieee.numeric_std.all ;
+
+entity mem_dados_tb is
+end entity ; -- mem_dados_tb
+architecture arch of mem_dados_tb is
+
+begin
+
+end architecture ; -- arch
