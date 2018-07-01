36d0c25a99942a0a5f0364fc887a3a47bbfacb15
Gastd <gabriel.araujo.5000@gmail.com>
2018-06-30 23:19:37 -0300

    Novo programa asm para test

---
+++ lab1_2.asm
@@ -0,0 +1,19 @@
+.data
+vetor:	.word 1 2 3 4 5 6 7 8 9
+tam:	.word 9
+
+.text
+main:	la $a0, vetor
+	la $a1, tam
+	lw $a1, 0($a1)
+
+pr_vet:	
+	move $t0, $a0	#inicializacao de variaveis
+L1:	
+	lw $a0, 0($t0)
+    lw $a0, 4($t0)
+    lw $a0, 8($t0)
+	lw $a0, 12($t0)
+	addi $t0, $t0, 4
+	addi $a1, $a1, -1
+	li $v0, 1
